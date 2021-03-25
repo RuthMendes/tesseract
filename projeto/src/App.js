44862c88ae9153de3fb50f948dfde349aa2c5b42
Ruth Mendes <mendessruth@gmail.com>
2021-03-25 16:35:09 -0300

    lista de membros com foto e nome

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,43 +1,43 @@
 import React, { useEffect, useState} from 'react'
 import './styles.css'
-import Input from './Components/Input'
+import SearchInput from './Components/SearchInput'
 
-
-
-const api = 'https://api.github.com/orgs/grupotesseract/public_members';
+const api = "https://api.github.com/orgs/grupotesseract/public_members";
 
 export default function App() {
-  const [info, setInfo] = useState({});
-  const [text, setText] = useState('');
+  const [info, setInfo] = useState([]);
+  const [text, setText] = useState("");
+  console.log(info);
 
   useEffect(() => {
-    if(text) {
-      fetch(`${api}members?filter[text]=&users/%7Buser%7D`)
-      .then((response) => response)
-      .then((response) => {
-        setInfo(response);
-        console.log(response)
-      });
-    }
-  }, [text]);
+    const findUsersOrg = async () => {
+      await fetch(api)
+        .then((response) => response.json())
+        .then((response) => {
+          setInfo(response);
+        });
+    };
+
+    findUsersOrg();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Members</h1>
-      <Input value={text} onChange={(search) => setText(search)}/>
-      {info.data && (
-        <ul>
-          {info.data.map((member) => (
-            <li key={member.login}></li>
+      <h1>Pesquise aqui!</h1>
+      <SearchInput value={text} onChange={(search) => setText(search)} />
+      <h2>Members List</h2>
+      {text && info && <span>Carregando...</span>}
+      {info && (
+        <ul className="members-list">
+          {info.map((member) => (
+            <li key={member.login}>
+              <img src={member.avatar_url} alt="avatar" />
+              <p>{member.login}</p>
               
-            
-
+            </li>
           ))}
         </ul>
       )}
-
     </div>
-  )
-
-
+  );
 }
